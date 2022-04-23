88f7625ec88896270d7f18a0e9594bac7b10485c
janV <102748690+janhvi0@users.noreply.github.com>
2022-04-23 13:46:31 +0530

    Add files via upload

---
+++ StudentAttendancePortal.py
@@ -0,0 +1,42 @@
+# STUDENT ATTENDANCE PORTAL------ to be complited
+print("~Studence Attendace Portal~")
+
+while(True):
+    print('Choose one of the following Operation:-\n'
+          '1. Give Attendance\n'
+          '2. Check Attendance')
+    choise = input('=')
+    if choise == '1':
+        stud_name = input('Enter Your name:-')
+        stud_rollno=input('Enter Your RollNo:-')
+        print('Your Attendance Is registered Successfully!!')
+        menu = input('Return to main menu(yes/no):-')
+        if menu == 'no':
+            print('ThankYou!!')
+            break
+
+        elif choise == '2':
+            list = [['janhvi', '111'], ['kim', '112'], ['park', '113'], ['jeon', '114'], ['min', '115'],
+                    ['jung', '116']]
+            sname = input('Enter student name:-')
+            if sname in list:
+                print(sname, 'is Present')
+            else:
+                print(sname, 'is Absent')
+
+            menu = input('Return to main menu(yes/no):-')
+            if menu == 'no':
+                print('ThankYou!!')
+                break
+
+
+
+
+
+
+
+
+
+
+
+
