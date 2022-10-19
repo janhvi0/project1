6278b47688a449af0eb2dc0377f68be648780cba
janV <janhvipatriwar7@gmail.com>
2022-10-19 12:24:45 +0530

    TOW BASIC PYTHON PROGRAM FOR PRACTICE

---
+++ Examples/ApniDictionary.py
@@ -0,0 +1,7 @@
+#example_______________________________(Mini dictionary)
+
+d1 = {"mutabal":"Which can be modified","immutable":"which cannot be modified","apple":"a fruit",
+      "carrot":"a vegetable","pen":"a thing used to write","monkey":"a animal"}
+inpt = input("Enter a word:-")
+print(d1[inpt])
+
